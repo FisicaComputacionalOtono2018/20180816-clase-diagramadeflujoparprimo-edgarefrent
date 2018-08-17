bfcccede6185dd76356c891f6a3bc0979b3acade
edgarefrent <42181839+edgarefrent@users.noreply.github.com>
2018-08-17 11:52:05 -0500

    Add files via upload

--- ejemplo.py
+++ ejemplo.py
@@ -1,8 +1,21 @@
-#Author:Tomay Tiburcio Edgar Efren
-#Fecha: 16 de agosto 2018
-#Intento
-s=12
-P=input ("Dame un numero: ")
-if P 
 
+
+def isPrimeFor(num):
+	flag=True
+	if num==1:
+		flag=False
+
+	for i in range (2, (num-1)):
+		if num%i==0:
+			flag=False
+			break
+	return flag
+
+p = input("Dame un numero: ")
+
+if isPrimeFor(p)==True:
+
+	print("Es primo")
+else :
+	print("No es primo")
 
